ce5f1c6785e34f2504a6a325843621aa2c257670
Laurent FINET <Laurent-finet@outlook.fr>
2026-03-05 22:11:33 +0100

    First commit

---
+++ PhishNet/Crawler.cs
@@ -0,0 +1,167 @@
+﻿using System.Text.RegularExpressions;
+
+namespace E_Crawl_CSharp
+{
+    public class Crawler
+    {
+        public Crawler(string DomainName, int Depth, int TargetMailCount, bool IncludeSubDomains, string Filter)
+        {
+            this.DomainName = DomainName;
+            this.Depth = Depth;          
+            this.TargetMailCount = TargetMailCount;
+            this.IncludeSubDomains = IncludeSubDomains;
+            this.Filter = Filter;
+
+            this.ExternalDomains = new List<string>();
+            this.EMails = new List<WebsiteEmail>();
+            this.VisitedURLs = new List<string>();
+            this.Completed = false;
+
+            this.ExternalDomains.Add(DomainName);
+        }
+
+        public List<WebsiteEmail> EMails { get; }
+        public bool Completed  { get; }
+        public string DomainName { get; }
+        public int Depth { get; }
+        public List<string> VisitedURLs { get; }
+        public int TargetMailCount { get; }
+        public bool IncludeSubDomains { get; }
+        public List<string> ExternalDomains { get; }
+        public string Filter { get; }
+
+        public async Task<List<WebsiteEmail>> Execute()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
+            client.DefaultRequestHeaders.Accept.ParseAdd("image/webp,*/*;q=0.8"); ;
+            client.DefaultRequestHeaders.AcceptLanguage.ParseAdd("en-US,en;q=0.9");
+            client.DefaultRequestHeaders.Referrer = new Uri("https://www.maisonsmoches.be/");
+
+            await recursivequeue(client, this.Depth, this.DomainName);
+            return this.EMails;
+        }
+
+        private Queue<WebsiteURL> _queue = new Queue<WebsiteURL>();
+        private async Task recursivequeue(HttpClient Client, int MaxDepth, string targetDomain)
+        {
+            _queue.Enqueue(new WebsiteURL(targetDomain, 1));
+            this.VisitedURLs.Add(targetDomain);
+
+            while (_queue.Count > 0)
+            {
+                WebsiteURL targetURL = _queue.Dequeue();                                      
+                string queryresponse = string.Empty;
+                try 
+                {
+                    CancellationTokenSource token = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+                    queryresponse = await Client.GetStringAsync(targetURL.URL, token.Token);
+                }
+                catch { continue; }
+
+                if (targetURL.Depth + 1 <= MaxDepth) {getURLsFromPage(queryresponse, targetURL.Depth); }
+                
+                // Scrape e-mails on the page
+                CancellationTokenSource timeOutToken = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+                Task<string[]> getMailsTask = Task.Run(() => getMailsFromPage(queryresponse));
+                Task completedTask = await Task.WhenAny(getMailsTask, Task.Delay(Timeout.Infinite, timeOutToken.Token));
+                if (completedTask != getMailsTask) 
+                {
+                    //Console.ForegroundColor = ConsoleColor.Red;
+                    //Console.WriteLine("[ERR0R] Mail regex function timed out (10s).");
+                    //Console.ResetColor(); continue;
+                }
+               
+                string[] mails = getMailsTask.Result;
+                if (mails.Length == 0) { continue; }
+
+                foreach (string mail in mails)
+                {
+                    if (string.IsNullOrWhiteSpace(mail)) { continue; }
+                    string cleanmail = mail;
+                    if (cleanmail.StartsWith("mailto:")) { cleanmail = cleanmail.Substring(7); }
+                    string[] allowedExtentions = [".be", ".com", ".eu", ".net", ".org", ".fr", ".ch", ".de", ".nl", ".it"];
+                    string mailExtension = cleanmail.Substring(cleanmail.LastIndexOf('.'));
+                    if (!allowedExtentions.Contains(mailExtension)) { continue; }
+                    if (!this.EMails.Any(e => e.Email == cleanmail)) { this.EMails.Add(new WebsiteEmail(cleanmail, targetURL.URL)); }
+                    if (this.TargetMailCount == EMails.Count) { return; }
+                }
+            }
+        }
+
+        private void getURLsFromPage(string Text, int Depth) {
+            Regex hrefregex = new Regex("href=[\"\\'](.*?)[\"\\']");
+            MatchCollection hrefs = hrefregex.Matches(Text);
+
+            foreach (Match match in hrefs)
+            {
+                string href = match.Value;
+                if (href.StartsWith("//")) { continue; } // invalid
+                if (href.StartsWith("href=")) { href = match.Value.ToString().Substring("href=".Length + 1, match.Value.ToString().Length - 1 - ("href=".Length + 1)); } // remove href=
+                if ((href.StartsWith("https://") || href.StartsWith("http://")) && !href.StartsWith(this.DomainName)) { // correct format + not in the current domain (external link)
+                    if (!string.IsNullOrWhiteSpace(this.Filter)) { if (!href.Contains(this.Filter)) { continue; } }
+                    appendNewDomain(href); continue; } // append to external links
+                if (href.StartsWith("/")) { href = this.DomainName.Substring(0, this.DomainName.Length - 1) + href; } // format properly        
+                if (!href.StartsWith(this.DomainName)) { continue; } // invalid
+                if (!this.VisitedURLs.Contains(href)) {
+                    VisitedURLs.Add(href);
+                    _queue.Enqueue(new WebsiteURL(href, Depth + 1));             
+                }
+            }
+        }
+
+        private string[] getMailsFromPage(string text)
+        {
+            Regex emailregex = new Regex(@"(?:mailto:)?([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})");
+            MatchCollection mails = emailregex.Matches(text);
+
+            List<string> emails = new List<string>();
+            foreach (Match mail in mails) { emails.Add(mail.Value); }
+            return emails.ToArray();
+        }
+
+        private void appendNewDomain(string href)
+        {
+            try
+            {
+                Uri domainUri = new Uri(href);
+                string domainroot = $"{domainUri.Scheme}://{domainUri.Host}/";
+
+                if (!this.IncludeSubDomains) 
+                { 
+                    if (domainUri.Host.Split('.').Length > 3) { return; }
+                    string domain = domainUri.Host.Split('.')[1];
+                    if (this.ExternalDomains.Any(e => e.Contains(domain))) { return; };
+                }
+
+                if (!this.ExternalDomains.Contains(domainroot)) { this.ExternalDomains.Add(domainroot); }
+            }
+            catch { }               
+        }
+    } 
+
+    public class WebsiteURL
+    {
+        public WebsiteURL(string URL, int Depth)
+        {
+            this.URL = URL;
+            this.Depth = Depth;
+        }
+
+        public string URL { get; set; }
+        public int Depth { get; set; }
+    }
+
+    public class WebsiteEmail
+    {
+        public WebsiteEmail(string Email, string URL)
+        {
+            this.Email = Email;
+            this.URL = URL;
+        }
+        
+        public string Email { get; }
+        public string URL { get; }   
+    
+    }
+}
---
+++ PhishNet/Program.cs
@@ -0,0 +1,298 @@
+﻿using E_Crawl_CSharp;
+using MySql.Data.MySqlClient;
+using System.Text;
+using System.Text.Json;
+
+class Program
+{
+    private static async Task Main(string[] args)
+    {
+        ApplicationOptions options = ValidateArguments(args);
+        switch (options.Type)
+        {
+            case "help": return; //DisplayHelp();
+            case "domain": await ScrapeSingleDomain(options); return;
+            case "domains": await ScrapeMultipleDomains(options); return;
+            default: throw new InvalidOperationException();
+        }     
+    }
+
+    private static ApplicationOptions ValidateArguments(string[] args)
+    {
+        if (args.Length < 1) { throw new ArgumentException(); }
+        if (string.IsNullOrWhiteSpace(args[0])) { throw new ArgumentException(); }
+        if (args[0] != "help" && args[0] != "domain" && args[0] != "domains") { throw new ArgumentException(); }      
+        ApplicationOptions options = new ApplicationOptions(args[0]); if (options.Type == "help") { return options; }
+
+        if (args.Length % 2 != 0) { throw new ArgumentException(); }
+        switch (options.Type)
+        {
+            case "domain":              
+                if (string.IsNullOrWhiteSpace(args[1])) { throw new ArgumentException(); }
+                options.Domain = args[1]; break;
+            case "domains":
+                if (string.IsNullOrWhiteSpace(args[1])) { throw new ArgumentException(); }               
+                if (File.Exists(args[1])) { throw new FileNotFoundException(); }
+                options.DomainList = args[1]; break;
+        }
+    
+        for (int i = 2; i <= args.Length - 1; i += 2)
+        {
+            switch (args[i])
+            {
+                case "-depth":
+                case "-d":
+                    if (string.IsNullOrWhiteSpace(args[i + 1])) { throw new ArgumentException(); }
+                    if (!int.TryParse(args[i + 1], out int _)) { throw new ArgumentException(); }
+                    if (int.Parse(args[i + 1]) == 0) { throw new ArgumentException(); }
+                    options.Depth = int.Parse(args[i + 1]); break;
+                
+                case "-maxemails":
+                case "-m":
+                    if (string.IsNullOrWhiteSpace(args[i + 1])) { throw new ArgumentException(); }
+                    if (!int.TryParse(args[i + 1], out int _)) { throw new ArgumentException(); }
+                    if (int.Parse(args[i + 1]) == 0) { throw new ArgumentException(); }
+                    options.MaxMails = int.Parse(args[i + 1]); break;
+               
+                case "-outfile":
+                case "-of":
+                    if (string.IsNullOrWhiteSpace(args[i + 1])) { throw new ArgumentException(); }
+                    options.OutputFile = args[i + 1]; break;
+               
+                case "-subdomains":
+                case "-sd":
+                    if (string.IsNullOrWhiteSpace(args[i + 1])) { throw new ArgumentException(); }
+                    if (!bool.TryParse(args[i + 1], out bool _)) { throw new ArgumentException(); }
+                    options.SubDomains = bool.Parse(args[i + 1]);  break;
+               
+                case "-threads":
+                case "-t":
+                    if (string.IsNullOrWhiteSpace(args[i + 1])) { throw new ArgumentException(); }
+                    if (!int.TryParse(args[i + 1], out int _)) { throw new ArgumentException(); }
+                    if (int.Parse(args[i + 1]) == 0) { throw new ArgumentException(); }
+                    options.Threads = int.Parse(args[i + 1]); break;
+               
+                case "-filter":
+                case "-f":
+                    if (string.IsNullOrWhiteSpace(args[i + 1])) { throw new ArgumentException(); }
+                    if (!args[i + 1].StartsWith(".")) { throw new ArgumentException(); }
+                    options.Filter = args[i + 1]; break;
+
+                case "-username":
+                    if (string.IsNullOrWhiteSpace(args[i + 1])) { throw new ArgumentException(); }
+                    options.UserName = args[i + 1]; break;
+
+                case "-password":
+                    if (string.IsNullOrWhiteSpace(args[i + 1])) { throw new ArgumentException(); }
+                    options.Password = args[i + 1]; break;
+
+                case "-database":
+                    if (string.IsNullOrWhiteSpace(args[i + 1])) { throw new ArgumentException(); }
+                    options.Database = args[i + 1]; break;
+
+                case "-address":
+                    if (string.IsNullOrWhiteSpace(args[i + 1])) { throw new ArgumentException(); }
+                    options.Address = args[i + 1]; break;
+
+                default: throw new ArgumentException();   
+            }
+        }
+
+        bool allSqlNullOrWhiteSpace = string.IsNullOrWhiteSpace(options.Address) && string.IsNullOrWhiteSpace(options.UserName) && string.IsNullOrWhiteSpace(options.Password) && string.IsNullOrWhiteSpace(options.Database);
+        bool noneSqlNullOrWhiteSpace = !string.IsNullOrWhiteSpace(options.Address) && !string.IsNullOrWhiteSpace(options.UserName) && !string.IsNullOrWhiteSpace(options.Password) && !string.IsNullOrWhiteSpace(options.Database);
+        if (!(allSqlNullOrWhiteSpace || noneSqlNullOrWhiteSpace)) { throw new ArgumentException(); }
+
+        return options;
+    }
+
+    private static async Task ScrapeSingleDomain(ApplicationOptions Options)
+    {
+        string domain = Options.Domain;
+        string? formatedDomainName = FormatDomainName(domain);
+        if (formatedDomainName == null) { return; }
+
+        try { await ScrapeDomains(formatedDomainName, Options); }
+        catch { Console.WriteLine("An error occured while attempting to scrape the domain: " + formatedDomainName); }
+    }
+
+    private static async Task ScrapeMultipleDomains(ApplicationOptions Options)
+    { 
+        int totalLines = File.ReadLines(Options.DomainList).Count(); int currentline = 1;
+        using (StreamReader reader = new StreamReader(Options.DomainList))
+        {
+            while (reader.EndOfStream == false)
+            {              
+                string domainName = reader.ReadLine() ?? string.Empty;
+                string? formatedDomainName = FormatDomainName(domainName);
+                if (formatedDomainName == null ) { return; }
+
+                try { await ScrapeDomains(formatedDomainName, Options); }
+                catch { Console.WriteLine("An error occured while attempting to scrape the domain: " + formatedDomainName); }
+
+                Console.WriteLine($"Progress: {currentline} of {totalLines} websites scraped so far...");
+                currentline += 1;
+            }
+        };
+    }
+
+    private static List<WebsiteEmail> _sqlBufferMailList = new List<WebsiteEmail>();
+    private static List<string> _sqlBufferDomainsList = new List<string>();
+    private static async Task ScrapeDomains(string StartingDomain, ApplicationOptions Options)
+    {
+        Queue<string> domainQueue = new Queue<string>(); domainQueue.Enqueue(StartingDomain);
+        List<string> visitedDomains = new List<string>(); visitedDomains.Add(StartingDomain);
+
+        List<Task<Crawler>> taskPool = new List<Task<Crawler>>();
+        for (byte i = 0; domainQueue.Count > 0; i ++)
+        {
+            string domain = domainQueue.Dequeue();
+            taskPool.Add(CrawlDomain(domain, Options.Depth, Options.MaxMails, Options.SubDomains, Options.Filter));
+        }
+
+        while (taskPool.Count > 0)
+        {
+            Task<Crawler> finishedTask = await Task.WhenAny(taskPool);
+            taskPool.Remove(finishedTask);
+
+            foreach (string externalDomain in finishedTask.Result.ExternalDomains)
+            {
+                if (visitedDomains.Contains(externalDomain)) { continue; }
+                visitedDomains.Add(externalDomain);
+                domainQueue.Enqueue(externalDomain);
+            }
+
+            if (finishedTask.Result.EMails.Count > 0 && !string.IsNullOrWhiteSpace(Options.OutputFile)) { AppendToFile(Options.OutputFile, finishedTask.Result.EMails); }
+
+            if (finishedTask.Result.EMails.Count > 0 && !string.IsNullOrWhiteSpace(Options.Address))
+            {
+                _sqlBufferMailList.AddRange(finishedTask.Result.EMails);
+                _sqlBufferDomainsList.AddRange(finishedTask.Result.VisitedURLs);
+
+                if (_sqlBufferMailList.Count > Options.Threads * 20)
+                {
+                    AppendToSQLDtb(Options.UserName, Options.Password, Options.Database, Options.Address, _sqlBufferMailList, _sqlBufferDomainsList);
+                    _sqlBufferMailList.Clear();
+                    _sqlBufferDomainsList.Clear();
+                }
+            }
+
+            while (taskPool.Count < Options.Threads && domainQueue.Count > 0)
+            {
+                string domain = domainQueue.Dequeue();
+                taskPool.Add(CrawlDomain(domain, Options.Depth, Options.MaxMails, Options.SubDomains, Options.Filter));
+            }
+
+            Console.WriteLine("Scraping completed for: " + finishedTask.Result.DomainName);
+            Console.WriteLine("Found e-mails         : " + finishedTask.Result.EMails.Count);
+            Console.WriteLine("Found new domains     : " + finishedTask.Result.ExternalDomains.Count);
+            Console.WriteLine("Active threads count  : " + taskPool.Count);
+            Console.WriteLine("Domain queue count    : " + domainQueue.Count + "\r\n");
+        }
+    }
+
+    static async Task<Crawler> CrawlDomain(string DomainName, int Depth, int TargetMailCount, bool IncludeSubDomains, string Filter)
+    {
+        Crawler crawler = new Crawler(DomainName, Depth, TargetMailCount, IncludeSubDomains, Filter);
+
+        try
+        {       
+            await crawler.Execute();       
+        } 
+        catch { }
+
+        return crawler;
+    }
+
+    private static string? FormatDomainName(string DomainName) 
+    {
+        try
+        {
+            DomainName = DomainName.Replace("\\", "/");
+            Uri newUri = new Uri(DomainName); string root = newUri.Host;
+            if (!root.StartsWith("www.")) { root = "www." + root; }
+            DomainName = "https://" + root + "/";
+            return DomainName;
+        }
+        catch (Exception ex)        
+        {
+            Console.WriteLine(ex.Message);
+            return null; 
+        }
+    }
+
+    private static void AppendToFile(string FullFileName, List<WebsiteEmail> EmailsList)
+    {
+        Dictionary<string, List<string>>? jsonMails = new Dictionary<string, List<string>>();
+        if (File.Exists(FullFileName))
+        {
+            jsonMails = JsonSerializer.Deserialize<Dictionary<string, List<string>>>(File.ReadAllText(FullFileName));
+            if (jsonMails == null) throw new NullReferenceException(nameof(jsonMails));
+        }
+
+        foreach (WebsiteEmail wsMail in EmailsList)
+        {
+            Uri domainUri = new Uri(wsMail.URL);
+            string domainRoot = $"{domainUri.Scheme}://{domainUri.Host}/";
+
+            if (!jsonMails.Keys.Contains(domainRoot)) { jsonMails.Add(domainRoot, new List<string> { wsMail.Email }); }           
+            else { if (!jsonMails[domainRoot].Contains(wsMail.Email)) { jsonMails[domainRoot].Add(wsMail.Email); } }
+        }
+
+        JsonSerializerOptions options = new JsonSerializerOptions(); options.WriteIndented = true;
+        File.WriteAllText(FullFileName, JsonSerializer.Serialize(jsonMails, options)); 
+    }
+
+    private static void AppendToSQLDtb(string Username, string Password, string Database,  string Address, List<WebsiteEmail> EmailsList, List<string> VisitedDomains)
+    {
+        string connectionString = $"server={Address};uid={Username};pwd={Password};database={Database};Convert Zero Datetime=True";
+        using (MySqlConnection conn = new MySqlConnection(connectionString))
+        {
+            conn.Open();
+            StringBuilder command = new StringBuilder();
+            command.AppendLine($"INSERT INTO mails (mail, website) VALUES");
+
+            for (int i = 0; i < EmailsList.Count; i++)
+            {
+                command.Append($"(@mail{i}, @website{i})");
+                if (i == EmailsList.Count - 1) { command.Append(";"); }
+                else { command.Append(","); }
+            }
+            MySqlCommand cmd = new MySqlCommand(command.ToString(), conn);
+            for (int i = 0; i < EmailsList.Count; i++)
+            {
+                cmd.Parameters.AddWithValue($"@mail{i}", EmailsList[i].Email);
+                cmd.Parameters.AddWithValue($"@website{i}", EmailsList[i].URL);
+            }
+            try { cmd.ExecuteNonQuery(); }
+            catch (Exception ex) { Console.WriteLine(ex); }
+
+            foreach (string domain in VisitedDomains)
+            {
+                cmd = new MySqlCommand("INSERT INTO visiteddomains VALUES (@domain, @date);", conn);
+                cmd.Parameters.AddWithValue("@domain", domain);
+                cmd.Parameters.AddWithValue("@date", DateTime.Now);
+                try { cmd.ExecuteNonQuery(); }
+                catch (Exception ex) { Console.WriteLine(ex); }
+            }
+        }
+    }
+
+    public class ApplicationOptions
+    {
+        public ApplicationOptions(string Type) { this.Type = Type; }
+
+        public string Type { get; set; }
+        public string Domain { get; set; } = string.Empty;
+        public string DomainList { get; set; } = string.Empty;
+        public int Depth { get; set; } = -1;
+        public int MaxMails { get; set; } = -1;
+        public string OutputFile { get; set; } = string.Empty;
+        public bool SubDomains { get; set; } = true;
+        public int Threads { get; set; } = 1;
+        public string Filter { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Database { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+    }
+}
